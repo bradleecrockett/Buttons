566404b20bfd94a782c2e9f5f3ab3a19adc64631
Brad C <bcrockett@rjuhsd.us>
2025-01-16 14:40:12 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+// Project Title - Buttons
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(600, 400);
+  background(235);
+  frameRate(10);
+  // rectMode(CENTER);
+}
+
+function draw() {
+  background(235);
+  noStroke();
+
+  rect(300, 200, 100, 50);
+
+  // text (mouseX + ", " + mouseY, 20,20);
+}
